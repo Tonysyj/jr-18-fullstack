c735b35cb75cc6cc95832b38592bf82dbe809efc
Tonysyj <sunyuejun8623@gmail.com>
2023-02-02 22:49:24 +1000

    file rename

--- P1/script.js
+++
@@ -1,7 +0,0 @@
-const menuToggle = document.querySelector('.toggle')
-const navBar = document.querySelector('.navBar')
-
-menuToggle.addEventListener('click', () => {
-  menuToggle.classList.toggle('active')
-  navBar.classList.toggle('active')
-})
